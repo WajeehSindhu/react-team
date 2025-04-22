c9da605b6793fa172b1e1b02946e291320d5cf63
Wajeeh Sindhu <wajeehsindhu@github.com>
2025-04-22 12:53:29 +0500

    Set Contact and Blog Page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import BlogSection from '../components/BlogSection'
 import Slider from "../components/Slider"
 import Services from '../components/Services'
 
@@ -26,60 +27,7 @@ const Blog = () => {
 
   <link rel="canonical" href="http://localhost:5173/blog" />
 </Helmet>
-      <section className="bg-gradient-to-br from-blue-300 via-blue-100 to-white dark:from-gray-900 dark:to-gray-800">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="-my-8 divide-y-2 divide-gray-100">
-            <div className="py-8 flex flex-wrap md:flex-nowrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="font-semibold title-font text-blue-700 dark:text-white">CATEGORY</span>
-                <span className="mt-1 text-gray-600 text-sm dark:text-gray-400">12 Jun 2019</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-blue-900 title-font mb-2 dark:text-white">Bitters hashtag waistcoat fashion axe chia unicorn</h2>
-                <p className="leading-relaxed text-gray-600 dark:text-gray-400">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-                <a className="text-indigo-500 inline-flex items-center mt-4 dark:text-indigo-300">Learn More
-                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-            </div>
-            <div className="py-8 flex flex-wrap md:flex-nowrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="font-semibold title-font text-blue-700 dark:text-white">CATEGORY</span>
-                <span className="mt-1 text-gray-600 text-sm dark:text-gray-400">12 Jun 2019</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-blue-900 title-font mb-2 dark:text-white">Meditation bushwick direct trade taxidermy shaman</h2>
-                <p className="leading-relaxed text-gray-600 dark:text-gray-400">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-                <a className="text-indigo-500 inline-flex items-center mt-4 dark:text-indigo-300">Learn More
-                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-            </div>
-            <div className="py-8 flex flex-wrap md:flex-nowrap">
-              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span className="font-semibold title-font text-blue-700 dark:text-white">CATEGORY</span>
-                <span className="text-sm text-gray-600 dark:text-gray-400">12 Jun 2019</span>
-              </div>
-              <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium text-blue-900 title-font mb-2 dark:text-white">Woke master cleanse drinking vinegar salvia</h2>
-                <p className="leading-relaxed text-gray-600 dark:text-gray-400">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-                <a className="text-indigo-500 inline-flex items-center mt-4 dark:text-indigo-300">Learn More
-                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <BlogSection/>
       <Slider />
       <Services/>
     </>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet';
+import ContactSection from '../components/ContactSection';
 import Banner from "../components/Banner"
 import ContactUs from '../components/ContactUs';
 
@@ -25,30 +26,7 @@ const Contact = () => {
 
   <link rel="canonical" href="http://localhost:5173/contact" />
 </Helmet>
-    <section className="bg-gradient-to-br from-blue-300 via-blue-100 to-white dark:from-gray-900 dark:to-gray-800">
-      <div className="max-w-7xl mx-auto px-6 py-20 grid grid-cols-1 md:grid-cols-2 items-center gap-10">
-       
-        <div className="space-y-6">
-          <h1 className="text-4xl md:text-5xl font-bold leading-tight text-blue-900 dark:text-white">
-            Learn React with Modern UI
-          </h1>
-          <p className="text-lg text-gray-700 dark:text-gray-300">
-            Build dynamic web apps using React and Tailwind CSS. Start your frontend journey today with this beginner-friendly course!
-            Both Android and iOS allow you to display formatted text by annotating ranges of a string with specific formatting like bold or colored text (NSAttributedString on iOS, SpannableString on Android). In practice, this is very tedious. For React Native, we decided to use the web paradigm for this, where you can nest text to achieve the same effect.
-          </p>
-          <button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg font-semibold transition duration-300">
-            Get Started
-          </button>
-        </div>
-        <div className="w-full">
-          <img
-            src="./images/contactBanner.png"
-            alt="React Banner"
-            className="w-full h-auto rounded-lg shadow-lg"
-          />
-        </div>
-      </div>
-    </section>
+   <ContactSection/>
     <Banner/>
     <ContactUs/>
     </>
